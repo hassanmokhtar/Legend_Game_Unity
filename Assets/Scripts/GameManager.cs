6868194a6e742db6f1d48d9cf25a20c466675377
hassanmokhtar <eng.hassan2015@yahoo.com>
2018-04-07 21:36:16 +0200

    update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	[SerializeField] GameObject player;
+	[SerializeField] GameObject[] spawnPoints;
+	[SerializeField] GameObject tanker;
+	[SerializeField] GameObject ranger;
+	[SerializeField] GameObject soldier;
+	[SerializeField] GameObject arrow ;
+	[SerializeField] Text levelText;
+	private GameObject newEnemy;
+
+	private bool gameOver = false;
+	private int currentLevel;
+	private float generatedSpawnTime = 1;
+	private float currentSpawnTime = 0;
+
+
+	private List<EnemyHealth> enemies = new List<EnemyHealth> ();
+	private List<EnemyHealth> killedEnemies = new List<EnemyHealth> ();
+
+	public void RegisterEnemy(EnemyHealth enemy){
+
+		enemies.Add (enemy);
+	}
+
+	public void killedEnemy(EnemyHealth enemy){
+
+		killedEnemies.Add (enemy);
+	}
+	public bool GameOver {
+		get {return gameOver; }
+	}
+
+	public GameObject Player {
+		get { return player; }
+	}
+
+	public GameObject Arrow {
+		get { return arrow; }
+	}
+	
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this){
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		// create reurcive function
+		StartCoroutine(spawn());
+
+		currentLevel = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		currentSpawnTime += Time.deltaTime;
+	}
+
+	public void PlayerHit(int currentHP) {
+
+		if (currentHP > 0) {
+			gameOver = false;
+		} else {
+			gameOver = true;
+			//StartCoroutine (endGame ("Defeat"));
+		}
+	}
+
+	IEnumerator spawn(){
+
+		// check if spawn time is greater than current
+		// then set current time spawn to 0
+		// and check if the count of enemies is less than current level 
+		// then get random number from 0 to spawn point count minus 1
+		// and then added the new number to spawnpoint to create new enemy
+
+		if(currentSpawnTime > generatedSpawnTime){
+
+			currentSpawnTime = 0;
+
+			if(enemies.Count < currentLevel){
+
+				int rand = Random.Range(0 , spawnPoints.Length - 1);
+
+				GameObject spawnLocation = spawnPoints[rand];
+
+				int randEnemy = Random.Range(0 , 3);
+
+				if(randEnemy == 0 )
+
+					newEnemy = Instantiate (soldier) as GameObject;
+
+				else if (randEnemy == 1)
+
+					newEnemy = Instantiate (ranger) as GameObject;	
+
+				else if (randEnemy == 2)
+
+					newEnemy = Instantiate (tanker) as GameObject;
+
+				newEnemy.transform.position = spawnLocation.transform.position;
+
+			}
+
+			if(killedEnemies.Count == currentLevel){
+
+				enemies.Clear();
+
+				killedEnemies.Clear();
+
+				yield return new WaitForSeconds(3f);
+
+				currentLevel ++;
+
+				levelText.text = "Level " + currentLevel;
+
+			}
+		}
+
+		yield return null;
+
+		// create reurcive function
+		StartCoroutine(spawn());
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    [SerializeField] private float moveSpeed = 10.0f;
+
+    [SerializeField] private LayerMask LayerMask;
+
+    private CharacterController CharacterController;
+
+    private Vector3 currentLookTarget = Vector3.zero;
+    private Animator anim;
+	private BoxCollider[] swordColliders;
+
+    // Use this for initialization
+    void Start () {
+        CharacterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+		swordColliders = GetComponentsInChildren<BoxCollider> ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!GameManager.instance.GameOver) {
+
+			Vector3 moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			CharacterController.SimpleMove (moveDirection * moveSpeed);
+
+			if (moveDirection == Vector3.zero) {
+				anim.SetBool ("IsWalking", false);
+			} else {
+				anim.SetBool ("IsWalking", true);
+			}
+
+			if (Input.GetMouseButtonDown (0)) {
+				anim.Play ("DoubleChop");
+			}
+
+			if (Input.GetMouseButtonDown (1)) {
+				anim.Play ("SpinAttack");
+			}
+		}
+				
+	}
+
+	// this is used for physics objects 
+     void FixedUpdate()
+    {
+		if (!GameManager.instance.GameOver) {
+			
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Debug.DrawRay (ray.origin, ray.direction * 500, Color.blue);
+
+
+			if (Physics.Raycast (ray, out hit, 500, LayerMask, QueryTriggerInteraction.Ignore)) {
+				
+				if (hit.point != currentLookTarget) {
+					currentLookTarget = hit.point; 
+				}
+
+				Vector3 targetposition = new Vector3 (hit.point.x, transform.position.y, hit.point.z);
+				Quaternion rotation = Quaternion.LookRotation (targetposition - transform.position);
+				transform.rotation = Quaternion.Lerp (transform.rotation, rotation, Time.deltaTime * 10f);
+			}
+		}
+    }
+
+	public void BeginAttack() {
+		foreach (var weapon in swordColliders) {
+			weapon.enabled = true;
+		}
+	}
+
+	public void EndAttack() {
+		foreach (var weapon in swordColliders) {
+			weapon.enabled = false;
+		}
+	}
+
+
+}
