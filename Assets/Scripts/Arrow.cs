3dda809a4546401b4b3136c015c12fa7cbb9d66d
hassanmokhtar <eng.hassan2015@yahoo.com>
2018-04-07 15:20:20 +0200

    tst

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision col){
+
+		Destroy (gameObject);
+	}
+}
+
---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.AI;
+
+
+public class EnemyMove : MonoBehaviour
+{
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+	private EnemyHealth enemyHealth;
+  
+    // Use this for initialization
+    void Start()
+    {
+		player = GameManager.instance.Player.transform;
+		enemyHealth = GetComponent<EnemyHealth> ();
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!GameManager.instance.GameOver && enemyHealth.IsAlive) {
+			nav.SetDestination (player.position);
+		} else if ((!GameManager.instance.GameOver || GameManager.instance.GameOver) && !enemyHealth.IsAlive) {
+			nav.enabled = false;
+		} else {
+			nav.enabled = false;
+			anim.Play ("Idle");
+		}
+    }
+}
